4a409f3fefb6b4e893b34d4fc7cb479f8b2c50ac
Eduardogmaas <eduardogmaas@gmail.com>
2024-03-15 20:41:21 -0300

    b

---
+++ alunos/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length=200)
+    data_nascimento = models.DateTimeField("date published")
